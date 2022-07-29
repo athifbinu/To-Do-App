a6cd4886f012ae9ee0bae8977adc2ca96dc196e3
Athif binu <binuathif@gmail.com>
2022-07-29 23:16:31 +0530

    to add delat  btn

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ btn.addEventListener("click",function(){
   let li=document.createElement("li");
   li.innerText=data;
   result.appendChild(li);
-  result.focus();
+  li.style.cssText="br";
+  result.focus();  
+
+
+  var button = document.createElement("button");
+  button.innerHTML = "Delat";
+  
+  // 2. Append somewhere
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(button);
+  
+  // 3. Add event handler
+  button.addEventListener ("click", function() {
+    
+  });
+
 
     datacolum.value='';     
 });
