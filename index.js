bdc28b2a45d6e2d940d216b9f08ca9f278b85b55
Athif binu <binuathif@gmail.com>
2022-07-30 15:37:19 +0530

    error

--- index.js
+++ index.js
@@ -12,17 +12,18 @@ btn.addEventListener("click",function(){
 
 
   var button = document.createElement("button");
-  button.innerHTML = "Delat";
+  button.innerHTML = "Deleat";
   
   // 2. Append somewhere
   var body = document.getElementsByTagName("body")[0];
   body.appendChild(button);
   
   // 3. Add event handler
-  button.addEventListener ("click", function() {
-    
-  });
 
+   button.addEventListener('click',function(){
+    li.result="";
+   })
 
     datacolum.value='';     
 });
+
